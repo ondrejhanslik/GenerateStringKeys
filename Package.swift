ed772bd7f29819c7cea38aa4c4efdc8ed193f17d
Ondřej Hanslík <ondrej.hanslik@gmail.com>
2022-03-20 20:52:10 +0100

    GenerateStringKeys is now prebuild

--- Package.swift
+++ Package.swift
@@ -3,15 +3,29 @@ import PackageDescription
 
 let package = Package(
     name: "LocalizationKeyPlugin",
+    // platforms: [.macOS(.v11)],
     products: [
+        /*
+        .executable(
+            name: "GenerateStringKeys",
+            targets: ["GenerateStringKeys"]
+        ),
+        */
         .plugin(
             name: "LocalizationKeyPlugin",
             targets: ["LocalizationKeyPlugin"]
         )
     ],
     targets: [
+        /*
         .executableTarget(
             name: "GenerateStringKeys"
+        ),
+        */
+        .binaryTarget(
+            name: "GenerateStringKeys",
+            url: "https://github.com/ondrejhanslik/GenerateStringKeys/releases/download/1.0.0/GenerateStringKeys.artifactbundle.zip",
+            checksum: "32d5a1cd19d59d81955373f183588ef290a43fc295920b7c37c8e52084c76f8c"
         ),
         .plugin(
             name: "LocalizationKeyPlugin",
