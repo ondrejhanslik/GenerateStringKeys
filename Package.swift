5e57a7ea6b525edd679b0c2e4fe7281f68729aca
Ondřej Hanslík <ondrej.hanslik@gmail.com>
2022-03-20 18:37:22 +0100

    Empty package

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "GenerateStringKeys",
+    targets: [
+        .executableTarget(
+            name: "GenerateStringKeys",
+            dependencies: []
+        )
+    ]
+)
